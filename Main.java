817b5ae7f34173464bbb60545388db4f3a6b1ec5
Shdd1 <129505192+Shdd1@users.noreply.github.com>
2024-07-02 15:37:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//Q1:
+     /*   Scanner input= new Scanner(System.in);
+        System.out.println("what is your role:");
+        String role=input.nextLine();
+        if(role.intern()=="admin"){
+            System.out.println("welcome admin" );
+        } else if ( role.intern()=="superuser") {
+            System.out.println( "welcome superuser" );
+        }
+        else System.out.println("welcome user");
+        */
+        //Q2
+
+    /*    Scanner input= new Scanner(System.in);
+        System.out.println("inter number 1:");
+        int num=input.nextInt();
+
+        System.out.println("inter number 2:");
+        int num2=input.nextInt();
+
+        System.out.println("inter number 3:");
+        int num3=input.nextInt();
+
+        if(num>num2&&num>num3){
+            System.out.println("The greatest:"+num);
+        }
+        else if (num2 >num&&num2>num3){
+            System.out.println("The greatest:"+num2);
+        }else
+            System.out.println("The greatest:"+num3);
+
+     */
+
+        //Q3
+     /*
+      Scanner input= new Scanner(System.in);
+        System.out.println(" number:");
+        int num=input.nextInt();
+       switch (num){
+           case 1:
+               System.out.println("Sunday");
+               break;
+           case 2:
+               System.out.println("Monday");
+               break;
+           case 3:
+               System.out.println("Tuesday");
+               break;
+           case 4:
+               System.out.println("Wednesday");
+               break;
+           case 5:
+               System.out.println("Thursday");
+               break;
+           case 6:
+               System.out.println("Friday");
+               break;
+           case 7:
+               System.out.println("Saturday");
+               break;
+           default:
+               System.out.println("plese inter betwen 1 and 7");
+                }
+                */
+
+         //Q4
+
+    /* Scanner input = new Scanner(System.in);
+        System.out.println("Enter your numeric score: ");
+        int degree = input.nextInt();
+        if (degree<=100 && degree>=90) {
+            System.out.println("A");
+        }
+           else if(degree>=80&& degree<89)
+        {
+            System.out.println("B");
+        }
+        else if(degree<=79&& degree>=70)
+        {
+            System.out.println("Letter Grade:C");
+        }
+
+        else if(degree>=60&& degree<=69)
+        {
+            System.out.println("Letter Grade:D");
+        }
+
+        else System.out.println("Letter Grade:F");
+        */
+
+        //Q5
+     /*    Scanner input=new Scanner(System.in);
+        System.out.println("enter your age:");
+        int age=input.nextInt();
+        if(age<13){
+            System.out.println("Child.");
+        } else if (age>=13 &&age<=19 ) {
+            System.out.println("Teenager.");
+        }
+        else System.out.println("Adult.");
+        */
+
+
+    }
+}
